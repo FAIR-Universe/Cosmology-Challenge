893621645463bd142a92ea35fbfe29e8e8ffbefd
Benjamin Thorne <bn.thorne@googlemail.com>
2023-08-16 16:28:30 -0700

    Tested mpi and egd

---
+++ scripts/tests/test_egd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --qos=debug
+#SBATCH -N 1
+#SBATCH -C cpu
+#SBATCH -t 00:30:00
+#SBATCH -J egd_test
+#SBATCH -o logs/egd_test-%j.txt
+
+srun -n 1 podman-hpc run --rm --volume /pscratch:/pscratch registry.nersc.gov/m4287/bthorne/fpm:latest python3 scripts/tests/test_egd.py
---
+++ scripts/tests/test_mpi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --qos=debug
+#SBATCH -N 2
+#SBATCH -C cpu
+#SBATCH -t 00:30:00
+#SBATCH -J mpi4py_test
+#SBATCH -o logs/mpi4py_test-%j.txt
+
+srun -n 64 podman-hpc run --rm --mpi registry.nersc.gov/m4287/bthorne/fpm:latest python3 scripts/tests/test_mpi.py
