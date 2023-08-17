3f4e61f3d815c34269121a370c4f9f0f0397f54b
Benjamin Thorne <bn.thorne@googlemail.com>
2023-08-17 11:07:09 -0700

    Add prints to check execution

--- scripts/tests/test_egd.py
+++ scripts/tests/test_egd.py
@@ -5,7 +5,10 @@ fastpm_tng_seed = BigFileCatalog(
     "/pscratch/sd/b/bthorne/fairuniverse/hsc_dataset/fastpm_box704/TNG_new_n4/Om_0.3089_S8_0.8159_0.2494",
     dataset="1",
 )
-fastpm_tng_seed.attrs["Nmesh"] = 256
+print(fastpm_tng_seed.comm.rank)
+
+fastpm_tng_seed.attrs["Nmesh"] = 128
 gamma = 1.1
 beta = 1.0
-EGD(fastpm_tng_seed, gamma, beta)
+disp = EGD(fastpm_tng_seed, gamma, beta)
+print(disp.shape)
