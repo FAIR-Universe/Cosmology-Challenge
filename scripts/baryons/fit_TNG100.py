e9a49f768917ea9d9fd537a4aa4a7f44132296ab
Benjamin Thorne <bn.thorne@googlemail.com>
2023-08-17 15:24:32 -0700

    Begin fitting script for EGD parameters

--- scripts/baryons/fit_TNG100.py
+++ scripts/baryons/fit_TNG100.py
@@ -1,32 +1,48 @@
-import argparse
-from pathlib import Path
+import pandas as pd
+import numpy as np
+
+"""Script assumes that the following /data directory
+is mounted.
+"""
+TNG100_RATIO_PATH = (
+    "/data/baryons/baryon-power-spectra/logPkRatio/logPkRatio_TNG100.dat"
+)
+
+FASTPM704_SNAPSHOTS_PATH = "/snapshot_dir/fastpm_box704/TNG_new_n4"
+FASTPM1536_SNAPSHOTS_PATH = "/snapshot_dir/fastpm_box1536/TNG"
+TNG100_REDSHIFTS = np.array(
+    [3.71, 3.49, 3.28, 2.90, 2.44, 2.1, 1.74, 1.41, 1.04, 0.7, 0.35, 0.18, 0.0]
+)
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--simulation", type=int, default=0)
-    parser.add_argument(
-        "--base_dir",
-        type=Path,
-        default="/pscratch/sd/b/bthorne/fairuniverse/hsc_dataset",
-    )
-    parser.add_argument("--data_dir", type=Path, default="data")
-    parser.add_argument("--cosmology_file", type=Path, default="data/cosmology.txt")
-    parser.add_argument("--redshifts_file", type=Path, default="data/redshifts.txt")
-    return vars(parser.parse_args())
+def get_TNG100_ratio():
+    return pd.read_csv(TNG100_RATIO_PATH, sep="\s+")
+
+
+def get_TNG100_redshifts():
+    return [f"z{redshift:.2f}".replace(".", "") for redshift in TNG100_REDSHIFTS]
 
 
 def get_covariance():
     return
 
 
-def fit_egd():
+def interpolate_TNG100_ratio(new_redshifts):
     return
 
 
 def main():
-    args = parse_arguments()
-    fit_egd(args)
+    # Interpolate TNG100 redshifts to match FASTPM redshifts.
+
+    # Interpolate TNG100 wavenumbers to match FASTPM wavenumbers.
+
+    # Calculate the target power spectrum (Pk_TNG100_ratio * Pk_FASTPM)
+
+    # Calculate the covariance matrix (assumed diagonal).
+
+    # Set up likelihood function for EGD parameters.
+
+    # Perform sampling.
     return
 
 
---
+++ scripts/baryons/fit_TNG100.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --qos=debug
+#SBATCH -N 1
+#SBATCH -C cpu
+#SBATCH -t 00:30:00
+#SBATCH -J fit_baryons
+#SBATCH -o logs/fit_baryons-%j.txt
+
+srun -n 1 podman-hpc run --mpi --rm \
+    --volume /pscratch/sd/b/bthorne/fairuniverse/hsc_dataset:/snapshot_dir \
+    --volume $PWD/scripts:/scripts \
+    --volume $PWD/data:/data \
+    fpm:latest python3 scripts/baryons/fit_TNG100.py \
+
