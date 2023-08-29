91ff2d2c68df4a3ed7b0b3b5700704c7d9f1a15e
Benjamin Thorne <bn.thorne@googlemail.com>
2023-08-29 13:57:56 -0700

    Add python script args to fit TNG

--- scripts/baryons/fit_TNG100.sh
+++ scripts/baryons/fit_TNG100.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-#SBATCH --qos=debug
-#SBATCH -N 4
+#SBATCH --qos=regular
+#SBATCH -N 8
 #SBATCH -C cpu
-#SBATCH -t 00:30:00
+#SBATCH -t 04:00:00
 #SBATCH -J fit_baryons
 #SBATCH -o logs/fit_baryons-%j.txt
 
 export OMP_NUM_THREADS=1
 total_processes=$((SLURM_JOB_NUM_NODES * 128))
 
-
 srun -n $total_processes podman-hpc run --mpi --rm                          \
     --volume /pscratch/sd/b/bthorne/fairuniverse/hsc_dataset:/snapshot_dir  \
     --volume $PWD/scripts:/scripts                                          \
     --volume $PWD/data:/data                                                \
     --volume $PWD/plots:/plots                                              \
-    fpm:latest python3 /scripts/baryons/fit_TNG100.py
+    fpm:latest python3 /scripts/baryons/fit_TNG100.py                       \
+    --output_stub kmax10_Nmesh1024_${SLURM_JOB_ID}                          \ 
+    --kmax 10                                                               \                  
+    --Nmesh 1024
 
